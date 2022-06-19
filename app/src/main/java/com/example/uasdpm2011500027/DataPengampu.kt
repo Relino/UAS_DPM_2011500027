24c6ce56bd77611db3f432783a21d9809dca73ad
RELINO <relinoabian@gmail.com>
2022-06-19 03:23:09 -0700

    Initial commit

---
+++ app/src/main/java/com/example/uasdpm2011500027/DataPengampu.kt
@@ -0,0 +1,11 @@
+package com.example.uasdpm2011500027
+
+class DataPengampu (
+    var nidn: String,
+    var nmDosen: String,
+    var jabatan: String,
+    var golonganpangkat: String,
+    var pendidikan: String,
+    var keahlian: String,
+    var programstudi: String,
+)
